8dd4d12bc1679c705f953fde54ddb0dc2a72afcc
maikx2333 <maikx@qq.com>
2022-03-31 11:52:10 +0800

    声音渐变

--- assets/scripts/framework/core/audio/AudioMusic.ts
+++ assets/scripts/framework/core/audio/AudioMusic.ts
@@ -1,15 +1,17 @@
-import { AudioClip, AudioSource, error, log, _decorator } from 'cc';
+import { AudioClip, AudioSource, error, log, Tween, tween, _decorator } from 'cc';
 import { ResourcesLoader } from '../../data/ResourcesLoader';
+import { audioMgr } from './AudioManager';
 const { ccclass, menu } = _decorator;
 
 /** 背景音乐 */
 @ccclass('AudioMusic')
 export class AudioMusic extends AudioSource {
     public onComplete: Function | null = null;
-
     private _progress: number = 0;
     private _url: string | null = null;
     private _isPlay: boolean = false;
+
+    private _tweenVo;
 
     /**
      * 设置音乐当前播放进度
@@ -26,19 +28,40 @@ export class AudioMusic extends AudioSource {
 
     public load(url: string,isLoop:boolean=true, callback?: Function) {
         ResourcesLoader.load(url, (data:AudioClip)=>{
-            if (this.playing) {
-                this.stop();
-                ResourcesLoader.release(this._url!);
-            }
+            this._fadeOut(()=>{
+                this.clip = data;
+                this.currentTime = 0;
+                this.play();
+                this.loop = isLoop;
+                callback && callback();
+                this._url = url;
 
-            this.clip = data;
-            this.currentTime = 0;
-            this.play();
-            this.loop = isLoop;
-            callback && callback();
-
-            this._url = url;
+                this._fadeIn()
+            })
         },AudioClip)
+    }
+
+    private _fadeOut(onFin:Function) {
+        if (this.playing) {
+            this._tweenVo?.stop();
+            this._tweenVo = new Tween(this);
+            this._tweenVo.to(1, {volume:0}, {onComplete:(tar)=>{
+                this._tweenVo = null
+                this.stop()
+                ResourcesLoader.release(this._url!);
+                onFin()
+            }}).start()
+        }
+        else {
+            onFin()
+        }
+    }
+
+    private _fadeIn() {
+        this._tweenVo?.stop();
+        this._tweenVo = new Tween(this);
+        this.volume = 0;
+        this._tweenVo.to(1, {volume:audioMgr.musicVolume}, {onComplete:()=>{this._tweenVo=null}}).start()
     }
 
     update(dt: number) {
