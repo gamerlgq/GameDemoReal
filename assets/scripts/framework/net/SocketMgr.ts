329ae70be3d5b8f15ee7641c1886271fee4e498a
maikx2333 <maikx@qq.com>
2022-04-01 15:55:36 +0800

    协议解析

--- assets/scripts/framework/net/SocketMgr.ts
+++ assets/scripts/framework/net/SocketMgr.ts
@@ -1,7 +1,10 @@
-import { log } from "cc";
+import { log, sys } from "cc";
+import { Proto } from "../../app/define/proto_mate";
 import { Singleton } from "../components/Singleton";
 import { gameMgr } from "../core/GameMgr";
 import { Message } from "../listener/Message";
+import { decodeUtf8, encodeUtf8, str2ab } from "../utils/functions";
+import Logger from "../utils/Logger";
 import { netLoadingMgr } from "./NetLoadingMgr";
 import { netStateMgr } from "./NetStateMgr";
 
@@ -14,14 +17,17 @@ import { netStateMgr } from "./NetStateMgr";
  */
 
 export type SocketCallback = {
-    (event: any):void
+    (event: any): void
 }
 
+const BUFF_SIZE = 1024 * 2;
 class SocketMgr extends Singleton {
     private _ws: WebSocket;
     private _StateChangeCallback: SocketCallback;
     private _ip: string;
     private _port: string;
+
+    private _sendBuffer: ArrayBuffer = new ArrayBuffer(BUFF_SIZE) //发送缓冲区
 
     private constructor() {
         super();
@@ -30,10 +36,10 @@ class SocketMgr extends Singleton {
             StateChangeCallback: this._listenOnSocketState.bind(this),
         };
         this.registerCallbackHandler(socketParams);
-        
+
     }
 
-    connect( ip: string,port: string,openFunc:SocketCallback,errorFunc:SocketCallback) {
+    connect(ip: string, port: string, openFunc: SocketCallback, errorFunc: SocketCallback) {
         this._ip = ip;
         this._port = port;
         try {
@@ -65,8 +71,8 @@ class SocketMgr extends Singleton {
             this.connect(
                 this._ip,
                 this._port,
-                (event) => {},
-                (event) => {log(event);}
+                (event) => { },
+                (event) => { log(event); }
             );
         }
     }
@@ -78,13 +84,137 @@ class SocketMgr extends Singleton {
         }
     }
 
-    send(msgId: number, data: Object = {}) {
-        data["proto"] = msgId;
-        data = JSON.stringify(data);
-        log("[WS] Send:", msgId, data);
-        this._ws.send(<string>data);
+    // send(msgId: number, data: Object = {}) {
+    //     data["proto"] = msgId;
+    //     data = JSON.stringify(data);
+    //     log("[WS] Send:", msgId, data);
+    //     this._ws.send(<string>data);
 
-        netLoadingMgr.addMsgLoading(msgId)
+    //     netLoadingMgr.addMsgLoading(msgId)
+    // }
+
+    send(msgId: number, ...sendParams: any[]) {
+        let msgParams = Proto.send[msgId]
+        if (!msgParams) {
+            Logger.e("没有此协议：" + msgId)
+            return
+        }
+
+        Logger.net("====>msg:" + msgId + " [" + sendParams + "]")
+
+        //发送字节流
+        let length = this._writeData(msgParams, sendParams)
+        if (length == 0) {
+            return
+        }
+
+        let newBuffer = this._sendBuffer.slice(0, length)
+        this._ws.send(newBuffer);
+    }
+
+    private _writeData(msgParams: Array<any>, ...sendParams: any[]) {
+        let length = 0;
+        let dataView = new DataView(this._sendBuffer)
+        for (let index = 0; index < msgParams.length; index++) {
+            const element = msgParams[index];
+            let typeStr = element.type
+            let value = sendParams[index]
+            if (typeStr == "uint8" || typeStr == "int8") {
+                dataView.setUint8(length, value)
+                length++
+            }
+            else if (typeStr == "uint16" || typeStr == "int16") {
+                dataView.setUint16(length, value, sys.isLittleEndian)
+                length += 2
+            }
+            else if (typeStr == "uint32" || typeStr == "int32") {
+                dataView.setUint32(length, value, sys.isLittleEndian)
+                length += 4
+            }
+            else if (typeStr == "string") {
+                let byteArray = encodeUtf8(value)
+                //写入长度
+                dataView.setUint32(length, byteArray.length, sys.isLittleEndian)
+                length += 4
+
+                //写入字节
+                byteArray.forEach((element, i) => {
+                    dataView.setUint8(length, element)
+                    length++
+                });
+            }
+            else if (typeStr == "array") {
+                this._writeData(element.fields, value)
+            }
+            else {
+                Logger.e("未知发送类型: " + typeStr)
+                return 0
+            }
+        }
+
+        return length
+    }
+
+    private _parseData(msgParams: Array<any>, arrayBuffer: ArrayBuffer, dataView: DataView, length: number, dataRet?) {
+        let dataParse = dataRet || {}
+
+        for (let index = 0; index < msgParams.length; index++) {
+            const element = msgParams[index];
+            let name = element.name
+            let typeStr = element.type
+            if (typeStr == "uint8" || typeStr == "int8") {
+                let num = dataView.getUint8(length)
+                dataParse[name] = num
+                length++
+            }
+
+            else if (typeStr == "int8") {
+                let num = dataView.getInt8(length)
+                dataParse[name] = num
+                length++
+            }
+
+            else if (typeStr == "uint16") {
+                let num = dataView.getUint16(length, sys.isLittleEndian)
+                dataParse[name] = num
+                length += 2
+            }
+
+            else if (typeStr == "int16") {
+                let num = dataView.getInt16(length, sys.isLittleEndian)
+                dataParse[name] = num
+                length += 2
+            }
+
+            else if (typeStr == "uint32") {
+                let num = dataView.getUint32(length, sys.isLittleEndian)
+                dataParse[name] = num
+                length += 4
+            }
+
+            else if (typeStr == "int32") {
+                let num = dataView.getInt32(length, sys.isLittleEndian)
+                dataParse[name] = num
+                length += 4
+            }
+
+            else if (typeStr == "string") {
+                let strLen = dataView.getUint32(length)
+                length += 4
+
+                let arrayBufferTemp = arrayBuffer.slice(length, strLen)
+                let str = decodeUtf8(arrayBufferTemp)
+                dataParse[name] = str
+            }
+            else if (typeStr == "array") {
+                dataParse = this._parseData(element.fields, arrayBuffer, dataView, length, dataParse)
+            }
+            else {
+                Logger.e("未知发送类型: " + typeStr)
+                return 0
+            }
+        }
+        return dataParse
     }
 
     sendInnerMsg(msgId: number, data: Object = {}) {
@@ -100,7 +230,7 @@ class SocketMgr extends Singleton {
     /**
      * 监听Socket 状态变化
      */
-     private _listenOnSocketState(event) {
+    private _listenOnSocketState(event) {
         netStateMgr.onSocketChange(event);
     }
 
@@ -119,12 +249,28 @@ class SocketMgr extends Singleton {
             log(event);
             return;
         }
-        let jsonData = JSON.parse(data);
-        log("[WS] Rev:", jsonData.proto, data);
-        let msg = new Message(-jsonData.proto, jsonData);
+        // let jsonData = JSON.parse(data);
+        // log("[WS] Rev:", jsonData.proto, data);
+
+        let length = 0
+        let arrayBuffer = str2ab(event.data)
+        let dataView = new DataView(arrayBuffer)
+        let msgId = dataView.getUint32(0, sys.isLittleEndian)
+        length += 4
+
+        let msgParams = Proto.recv[msgId]
+        if (!msgParams) {
+            Logger.e("Net <====: 没有此id的协议[" + msgId + "]")
+            return
+        }
+
+        let dataParse = this._parseData(msgParams, arrayBuffer, dataView, length)
+        let msg = new Message(msgId, dataParse);
         gameMgr.addNetMessage(msg);
 
         netLoadingMgr.removeMsgLoading(msg.msgId)
+
+        Logger.net("<==== msgId:" + msgId + '['+dataParse+']')
     }
 
     private _onerror(event) {
@@ -148,6 +294,6 @@ class SocketMgr extends Singleton {
 }
 
 // ()();
-export let socketMgr = (()=>{
+export let socketMgr = (() => {
     return SocketMgr.getInstance<SocketMgr>();
 })();
