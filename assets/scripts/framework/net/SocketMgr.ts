570f82099a6fc460abffdb292ac94050be1efe88
maikx2333 <maikx@qq.com>
2022-04-01 17:20:12 +0800

    协议解析

--- assets/scripts/framework/net/SocketMgr.ts
+++ assets/scripts/framework/net/SocketMgr.ts
@@ -5,6 +5,7 @@ import { gameMgr } from "../core/GameMgr";
 import { Message } from "../listener/Message";
 import { decodeUtf8, encodeUtf8, str2ab } from "../utils/functions";
 import Logger from "../utils/Logger";
+import BufferParser from "./BufferParser";
 import { netLoadingMgr } from "./NetLoadingMgr";
 import { netStateMgr } from "./NetStateMgr";
 
@@ -94,128 +95,17 @@ class SocketMgr extends Singleton {
     // }
 
     send(msgId: number, ...sendParams: any[]) {
-        let msgParams = Proto.send[msgId]
-        if (!msgParams) {
-            Logger.e("没有此协议：" + msgId)
+       
+        let bufferParser = new BufferParser
+        let arrbuffSend = bufferParser.generateMsgArrayBuffer(msgId, sendParams)
+        if (!arrbuffSend) {
             return
         }
 
+        this._ws.send(arrbuffSend);
         Logger.net("====>msg:" + msgId + " [" + sendParams + "]")
-
-        //发送字节流
-        let length = this._writeData(msgParams, sendParams)
-        if (length == 0) {
-            return
-        }
-
-        let newBuffer = this._sendBuffer.slice(0, length)
-        this._ws.send(newBuffer);
     }
 
-    private _writeData(msgParams: Array<any>, ...sendParams: any[]) {
-        let length = 0;
-        let dataView = new DataView(this._sendBuffer)
-        for (let index = 0; index < msgParams.length; index++) {
-            const element = msgParams[index];
-            let typeStr = element.type
-            let value = sendParams[index]
-            if (typeStr == "uint8" || typeStr == "int8") {
-                dataView.setUint8(length, value)
-                length++
-            }
-            else if (typeStr == "uint16" || typeStr == "int16") {
-                dataView.setUint16(length, value, sys.isLittleEndian)
-                length += 2
-            }
-            else if (typeStr == "uint32" || typeStr == "int32") {
-                dataView.setUint32(length, value, sys.isLittleEndian)
-                length += 4
-            }
-            else if (typeStr == "string") {
-                let byteArray = encodeUtf8(value)
-                //写入长度
-                dataView.setUint32(length, byteArray.length, sys.isLittleEndian)
-                length += 4
-
-                //写入字节
-                byteArray.forEach((element, i) => {
-                    dataView.setUint8(length, element)
-                    length++
-                });
-            }
-            else if (typeStr == "array") {
-                this._writeData(element.fields, value)
-            }
-            else {
-                Logger.e("未知发送类型: " + typeStr)
-                return 0
-            }
-        }
-
-        return length
-    }
-
-    private _parseData(msgParams: Array<any>, arrayBuffer: ArrayBuffer, dataView: DataView, length: number, dataRet?) {
-        let dataParse = dataRet || {}
-
-        for (let index = 0; index < msgParams.length; index++) {
-            const element = msgParams[index];
-            let name = element.name
-            let typeStr = element.type
-            if (typeStr == "uint8" || typeStr == "int8") {
-                let num = dataView.getUint8(length)
-                dataParse[name] = num
-                length++
-            }
-
-            else if (typeStr == "int8") {
-                let num = dataView.getInt8(length)
-                dataParse[name] = num
-                length++
-            }
-
-            else if (typeStr == "uint16") {
-                let num = dataView.getUint16(length, sys.isLittleEndian)
-                dataParse[name] = num
-                length += 2
-            }
-
-            else if (typeStr == "int16") {
-                let num = dataView.getInt16(length, sys.isLittleEndian)
-                dataParse[name] = num
-                length += 2
-            }
-
-            else if (typeStr == "uint32") {
-                let num = dataView.getUint32(length, sys.isLittleEndian)
-                dataParse[name] = num
-                length += 4
-            }
-
-            else if (typeStr == "int32") {
-                let num = dataView.getInt32(length, sys.isLittleEndian)
-                dataParse[name] = num
-                length += 4
-            }
-
-            else if (typeStr == "string") {
-                let strLen = dataView.getUint32(length)
-                length += 4
-
-                let arrayBufferTemp = arrayBuffer.slice(length, strLen)
-                let str = decodeUtf8(arrayBufferTemp)
-                dataParse[name] = str
-            }
-            else if (typeStr == "array") {
-                dataParse = this._parseData(element.fields, arrayBuffer, dataView, length, dataParse)
-            }
-            else {
-                Logger.e("未知发送类型: " + typeStr)
-                return 0
-            }
-        }
-        return dataParse
-    }
 
     sendInnerMsg(msgId: number, data: Object = {}) {
         let msg = new Message(msgId, data);
@@ -249,28 +139,14 @@ class SocketMgr extends Singleton {
             log(event);
             return;
         }
-        // let jsonData = JSON.parse(data);
-        // log("[WS] Rev:", jsonData.proto, data);
 
-        let length = 0
-        let arrayBuffer = str2ab(event.data)
-        let dataView = new DataView(arrayBuffer)
-        let msgId = dataView.getUint32(0, sys.isLittleEndian)
-        length += 4
-
-        let msgParams = Proto.recv[msgId]
-        if (!msgParams) {
-            Logger.e("Net <====: 没有此id的协议[" + msgId + "]")
-            return
-        }
-
-        let dataParse = this._parseData(msgParams, arrayBuffer, dataView, length)
-        let msg = new Message(msgId, dataParse);
+        let bufferParser = new BufferParser()
+        let dataParse = bufferParser.generateParseData(data)
+        let msg = new Message(bufferParser.msgId, dataParse);
         gameMgr.addNetMessage(msg);
 
         netLoadingMgr.removeMsgLoading(msg.msgId)
-
-        Logger.net("<==== msgId:" + msgId + '['+dataParse+']')
+        Logger.net("<==== msgId:" + bufferParser.msgId + '['+dataParse+']')
     }
 
     private _onerror(event) {
